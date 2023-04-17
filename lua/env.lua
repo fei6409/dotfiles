a6cbc846e1a078241e30f8a41b87b29d9ffc7874
fei6409 <fei6409@gmail.com>
2023-04-18 01:28:17 +0800

    lua: Use alias for configs

--- lua/env.lua
+++ lua/env.lua
@@ -1,58 +1,60 @@
 -- vim: foldmethod=marker
 
 -- General options
-vim.opt.breakindent = true -- indent the wrapped lines
-vim.opt.cmdheight = 2 -- more lines for command line
-vim.opt.confirm = true -- confirm on quitting without saving
-vim.opt.cursorline = true -- highlight line of the cursor
-vim.opt.foldmethod = 'marker' -- use markers ('{{{' and '}}}') to specify folds
-vim.opt.ignorecase = true -- case-insensitive search
-vim.opt.list = true -- list mode for special characters
-vim.opt.listchars = {tab='» ', trail='·', nbsp='%'} -- list mode characters
-vim.opt.mouse = '' -- mouse disabled
-vim.opt.number = true -- line number
-vim.opt.scrolloff = 5 -- minimal number of lines to keep above and below the cursor
-vim.opt.smartcase = true -- case-sensitive search if uppercase included
-vim.opt.spelllang = 'en_us' -- spellchecking language
-vim.opt.syntax = 'on' -- syntax highlighting
-vim.opt.termguicolors = true -- 24-bit RGB color
-vim.opt.textwidth = 80 -- wrap long lines
-vim.opt.ttimeoutlen = 5 -- shorten the wait time of key code sequence for faster <esc> response
-vim.opt.undofile = true -- save undo history
+local opt = vim.opt
+opt.breakindent = true -- indent the wrapped lines
+opt.cmdheight = 2 -- more lines for command line
+opt.confirm = true -- confirm on quitting without saving
+opt.cursorline = true -- highlight line of the cursor
+opt.foldmethod = 'marker' -- use markers ('{{{' and '}}}') to specify folds
+opt.ignorecase = true -- case-insensitive search
+opt.list = true -- list mode for special characters
+opt.listchars = {tab='» ', trail='·', nbsp='%'} -- list mode characters
+opt.mouse = '' -- mouse disabled
+opt.number = true -- line number
+opt.scrolloff = 5 -- minimal number of lines to keep above and below the cursor
+opt.smartcase = true -- case-sensitive search if uppercase included
+opt.spelllang = 'en_us' -- spellchecking language
+opt.syntax = 'on' -- syntax highlighting
+opt.termguicolors = true -- 24-bit RGB color
+opt.textwidth = 80 -- wrap long lines
+opt.ttimeoutlen = 5 -- shorten the wait time of key code sequence for faster <esc> response
+opt.undofile = true -- save undo history
 -- indent related {{{
-vim.opt.expandtab = true -- expand tab
-vim.opt.shiftwidth = 2 -- shift width
-vim.opt.softtabstop = -1 -- when sts is negative, the value of 'shiftwidth' is used
-vim.opt.tabstop = 2 -- tab width
-vim.opt.cindent = true -- experimental
-vim.opt.smartindent = true -- experimental
+opt.expandtab = true -- expand tab
+opt.shiftwidth = 2 -- shift width
+opt.softtabstop = -1 -- when sts is negative, the value of 'shiftwidth' is used
+opt.tabstop = 2 -- tab width
+opt.cindent = true -- experimental
+opt.smartindent = true -- experimental
 --- }}}
 
 -- Key bindings
-vim.keymap.set({'n','v'}, ';', ':')
-vim.keymap.set('n', 'q:', ':q') -- no more command history on typo
+local keyset = vim.keymap.set
+keyset({'n','v'}, ';', ':')
+keyset('n', 'q:', ':q') -- no more command history on typo
 
 -- "_ is a blackhole register, use it for operations w/o changing the default register
-vim.keymap.set('v', 'p', '"_dP') -- paste
-vim.keymap.set({'n','v'}, 'x', '"_x') -- delete
-vim.keymap.set({'n','v'}, 's', '"_s') -- replace
+keyset('v', 'p', '"_dP') -- paste
+keyset({'n','v'}, 'x', '"_x') -- delete
+keyset({'n','v'}, 's', '"_s') -- replace
 
-vim.keymap.set('n', '<TAB>', ':bnext<CR>') -- next buffer
-vim.keymap.set('n', '<S-TAB>', ':bprevious<CR>') -- previous buffer
+keyset('n', '<TAB>', ':bnext<CR>') -- next buffer
+keyset('n', '<S-TAB>', ':bprevious<CR>') -- previous buffer
 
-vim.keymap.set('n', '<leader>l', ':nohlsearch<CR>') -- close highlight search
-vim.keymap.set('n', '<leader>q', ':bp<BAR>bw #<CR>') -- delete current buffer
-vim.keymap.set('n', '<leader>r', 'zR<CR>') -- unfold all foldings
-vim.keymap.set('n', '<leader>s', ':Rg <C-R><C-W><CR>') -- unfold all foldings
-vim.keymap.set('n', '<leader>j', ':%!python -m json.tool<CR>') -- prettify json
+keyset('n', '<leader>l', ':nohlsearch<CR>') -- close highlight search
+keyset('n', '<leader>q', ':bp<BAR>bw #<CR>') -- delete current buffer
+keyset('n', '<leader>r', 'zR<CR>') -- unfold all foldings
+keyset('n', '<leader>s', ':Rg <C-R><C-W><CR>') -- unfold all foldings
+keyset('n', '<leader>j', ':%!python -m json.tool<CR>') -- prettify json
 
 -- copy/paste with system clipboard
-vim.keymap.set('n', '<leader>y', '"+yiw')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('n', '<leader>P', '"+P')
+keyset('n', '<leader>y', '"+yiw')
+keyset('v', '<leader>y', '"+y')
+keyset('n', '<leader>p', '"+p')
+keyset('n', '<leader>P', '"+P')
 
-vim.keymap.set('n', '<F9>', ':set invnumber invlist<CR>') -- toggle line number and list mode
+keyset('n', '<F9>', ':set invnumber invlist<CR>') -- toggle line number and list mode
 
 -- autocmd
 local augroup = vim.api.nvim_create_augroup('UserCmds', { clear=true })
