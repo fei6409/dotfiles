86a82fe45242aae3d0de089f8f12c25ec2815f7d
fei6409 <fei6409@gmail.com>
2023-04-22 13:48:27 +0800

    lua: Configure indent-blankline.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -185,6 +185,26 @@ return {
       }
     end,
   },
+  -- visual indentation guide
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      vim.cmd[[highlight IndentBlanklineIndent1 guibg=#181818 gui=nocombine]]
+      vim.cmd[[highlight IndentBlanklineIndent2 guibg=#0d0c0c gui=nocombine]]
+      require('indent_blankline').setup {
+        char = '',
+        use_treesitter = true,
+        char_highlight_list = {
+          'IndentBlanklineIndent1',
+          'IndentBlanklineIndent2',
+        },
+        space_char_highlight_list = {
+          'IndentBlanklineIndent1',
+          'IndentBlanklineIndent2',
+        },
+      }
+    end,
+  },
   -- mainly for the git blame feature
   {
     'tpope/vim-fugitive',
