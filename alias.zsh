0a1670e5194bc3237b8a88b7ebf9ac0d454d3089
Fei Shao <fshao@chromium.org>
2023-05-23 16:29:07 +0800

    feat(alias): Add upstream alias

--- alias.zsh
+++ alias.zsh
@@ -94,6 +94,7 @@ if ([[ -f /etc/lsb-release ]] && grep "GOOGLE_ID=Goobuntu" /etc/lsb-release >/de
 
   alias rs='repo sync .'
   alias rsa='repo sync -j 16 -n && repo sync -j 16 -l'
+  alias upstream='cd ${HOME}/chromiumos/src/third_party/kernel/upstream'
   alias ker419='cd ${HOME}/chromiumos/src/third_party/kernel/v4.19'
   alias ker54='cd ${HOME}/chromiumos/src/third_party/kernel/v5.4'
   alias ker510='cd ${HOME}/chromiumos/src/third_party/kernel/v5.10'
