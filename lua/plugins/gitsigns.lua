b475480b75bccb7829e1a28c8b7a8b6c5a0e0878
fei6409 <fei6409@gmail.com>
2023-06-21 23:04:35 +0800

    feat(lua): Add gitsigns.nvim

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+-- gitsigns.nvim
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup {
+    }
+    local keyset = vim.keymap.set
+    local gs = package.loaded.gitsigns
+    keyset('n', '<leader>gb', gs.toggle_current_line_blame)
+  end,
+}
