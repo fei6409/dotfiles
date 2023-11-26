a5cba34c80fddd4939ff9b3a8831562777f5af79
fei6409 <fei6409@gmail.com>
2023-11-26 15:40:37 +0800

    style(plugins): Improve telescope readability

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
         },
     },
     config = function()
+        local tel_builtin = require('telescope.builtin')
         -- Inspired by https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes
         local function is_git_repo()
             vim.fn.system('git rev-parse --is-inside-work-tree')
@@ -22,25 +23,20 @@ return {
         local project_files = function()
             local opts = {}
             if is_git_repo() then opts = { cwd = get_git_root() } end
-            require('telescope.builtin').find_files(opts)
+            tel_builtin.find_files(opts)
         end
 
         local keyset = vim.keymap.set
-        keyset('n', '<leader>ss', require('telescope.builtin').grep_string,
-            { desc = '[S]earch current [S]tring' })
-        keyset('n', '<leader>sg', require('telescope.builtin').live_grep,
-            { desc = '[S]earch by rip[G]rep' })
-        keyset('n', '<leader>sh', require('telescope.builtin').help_tags,
-            { desc = '[S]earch [H]elp' })
-        keyset('n', '<leader>sk', require('telescope.builtin').keymaps,
-            { desc = '[S]earch [K]eymaps' })
-        keyset('n', '<leader>sd', require('telescope.builtin').diagnostics,
-            { desc = '[S]earch [D]iagnostics' })
+        keyset('n', '<leader>ss', tel_builtin.grep_string, { desc = '[S]earch current [S]tring' })
+        keyset('n', '<leader>sg', tel_builtin.live_grep, { desc = '[S]earch by rip[G]rep' })
+        keyset('n', '<leader>sh', tel_builtin.help_tags, { desc = '[S]earch [H]elp' })
+        keyset('n', '<leader>sk', tel_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        keyset('n', '<leader>sd', tel_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         keyset('n', '<leader>sf', project_files,
             { desc = '[S]earch [F]iles Modified: search from repo root when in a Git repo' })
         keyset('n', '<leader>s/', function()
                 local opt = require('telescope.themes').get_dropdown { previewer = false, }
-                require('telescope.builtin').current_buffer_fuzzy_find(opt)
+                tel_builtin.current_buffer_fuzzy_find(opt)
             end,
             { desc = '[S]earch in [/]current buffer' })
 
