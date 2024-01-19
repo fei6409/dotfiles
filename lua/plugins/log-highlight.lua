1807b32a3eb1c9b3de8287336bc01f7242d5f61b
fei6409 <fei6409@gmail.com>
2024-01-19 17:56:29 +0800

    feat(plugins): Allow more log patterns

--- lua/plugins/log-highlight.lua
+++ lua/plugins/log-highlight.lua
@@ -11,6 +11,7 @@ return {
             pattern = {
                 '/var/log/.*',
                 'messages%..*',
+                'messages-.*',
                 '.*%.LATEST',
                 '.*%.PREVIOUS',
                 '.*%.previous',
