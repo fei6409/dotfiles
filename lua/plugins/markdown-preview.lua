b7ea2e81c874a06d717feb08b87bf57e6310569e
fei6409 <fei6409@gmail.com>
2024-05-24 23:51:26 +0800

    fix(plugin): Fix markdown-preview lazy update error

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,11 @@
 -- markdown-preview.nvim
 return {
     'iamcco/markdown-preview.nvim',
-    build = 'cd app && npm install',
+    -- Fix Lazy Update error (https://github.com/iamcco/markdown-preview.nvim/issues/616):
+    -- You have local changes in `xxx/nvim/lazy/markdown-preview.nvim`:
+    --   * app/yarn.lock
+    -- Please remove them to update.
+    build = 'cd app && npm install && git restore .',
     config = function()
         vim.g.mkdp_filetypes = { 'markdown' }
         vim.g.mkdp_page_title = '**${name}**'
