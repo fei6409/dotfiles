df85975e4c4a2bc11d028787663d3ae8af40e749
fei6409 <fei6409@gmail.com>
2024-05-28 01:10:27 +0800

    style(plugins): Replace 'config' with 'opts' in Lazy.nvim setup

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -5,5 +5,5 @@ return {
         { '<leader>sl', '<Plug>(leap-forward-to)', mode = { 'n', 'x', 'o' }, desc = 'Leap forward to' },
         { '<leader>sL', '<Plug>(leap-backward-to)', mode = { 'n', 'x', 'o' }, desc = 'Leap backward to' },
     },
-    config = true,
+    opts = {},
 }
--- lua/plugins/log-highlight.lua
+++ lua/plugins/log-highlight.lua
@@ -1,22 +1,20 @@
 -- log-highlight
 return {
     'fei6409/log-highlight.nvim',
-    config = function()
-        require('log-highlight').setup {
-            extension = 'log',
-            filename = {
-                'messages',
-                'logcat',
-                'syslog',
-            },
-            pattern = {
-                '/var/log/.*',
-                'messages%..*',
-                'messages-.*',
-                '.*%.LATEST',
-                '.*%.PREVIOUS',
-                '.*%.previous',
-            },
-        }
-    end,
+    opts = {
+        extension = 'log',
+        filename = {
+            'messages',
+            'logcat',
+            'syslog',
+        },
+        pattern = {
+            '/var/log/.*',
+            'messages%..*',
+            'messages-.*',
+            '.*%.LATEST',
+            '.*%.PREVIOUS',
+            '.*%.previous',
+        },
+    },
 }
