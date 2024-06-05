0939e27b5b60866a179b1984a5f10abe8e1f36b5
fei6409 <fei6409@gmail.com>
2024-06-05 22:42:20 +0800

    feat(plugins): Add flash.nvim

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+-- Enhanced search labels and motions
+return {
+    'folke/flash.nvim',
+    opts = {},
+    keys = {
+        { '<C-f>', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end,   desc = 'Flash Search' },
+        { '<C-f>', mode = { 'c' },           function() require('flash').toggle() end, desc = 'Toggle Flash Search' },
+    },
+}
