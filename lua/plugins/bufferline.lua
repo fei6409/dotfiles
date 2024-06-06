8a310b5a7f1665de08ae7f7c9120534b3f66bccf
fei6409 <fei6409@gmail.com>
2024-06-06 19:09:43 +0800

    feat(plugins): Enable bufferline.nvim

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,21 +1,37 @@
--- buffer line
+-- Tabline support
 return {
     'akinsho/bufferline.nvim',
     version = '*',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('bufferline').setup {
-            options = {
-                modified_icon = '*',
-                show_buffer_icons = false,
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-            },
-            highlights = {
-                buffer_selected = { italic = false },
-                diagnostic_selected = { italic = false },
-            },
-        }
-    end,
-    enabled = false,
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        -- Seems mason.nvim needs to go first, or race can happen and lead to
+        -- following errors:
+        -- Spawning language server with cmd: `xxx-server` failed.
+        -- The language server is either not installed, missing from PATH, or
+        -- not executable.
+        'williamboman/mason.nvim',
+    },
+    -- Ignore 'keys' property and always load the plugin on start.
+    lazy = false,
+    opts = {
+        options = {
+            show_buffer_close_icons = false,
+            tab_size = 10,
+        },
+        highlights = {
+            buffer_selected = { bold = true, italic = false },
+        },
+    },
+    keys = {
+        { '<A-1>', '<cmd>BufferLineGoToBuffer 1<CR>', silent = true },
+        { '<A-2>', '<cmd>BufferLineGoToBuffer 2<CR>', silent = true },
+        { '<A-3>', '<cmd>BufferLineGoToBuffer 3<CR>', silent = true },
+        { '<A-4>', '<cmd>BufferLineGoToBuffer 4<CR>', silent = true },
+        { '<A-5>', '<cmd>BufferLineGoToBuffer 5<CR>', silent = true },
+        { '<A-6>', '<cmd>BufferLineGoToBuffer 6<CR>', silent = true },
+        { '<A-7>', '<cmd>BufferLineGoToBuffer 7<CR>', silent = true },
+        { '<A-8>', '<cmd>BufferLineGoToBuffer 8<CR>', silent = true },
+        { '<A-9>', '<cmd>BufferLineGoToBuffer 9<CR>', silent = true },
+        { '<A-0>', '<cmd>BufferLineGoToBuffer 0<CR>', silent = true },
+    },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,14 +20,14 @@ return {
             lualine_y = { 'filetype' },
             lualine_z = { 'progress', 'location' },
         },
-        tabline = {
-            lualine_a = {
-                {
-                    'buffers',
-                    icons_enabled = false,
-                    symbols = { modified = '*', alternate_file = '' },
-                },
-            },
-        },
+        -- tabline = {
+        --     lualine_a = {
+        --         {
+        --             'buffers',
+        --             icons_enabled = false,
+        --             symbols = { modified = '*', alternate_file = '' },
+        --         },
+        --     },
+        -- },
     },
 }
