9ca2075bd3e354a50c086070e06cad92acfff9ce
fei6409 <fei6409@gmail.com>
2024-06-08 00:46:16 +0800

    feat(plugins): cmp: Clean up and tune enter behavior

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,8 +6,6 @@ return {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-nvim-lsp',
-        -- for Lua development in nvim
-        'hrsh7th/cmp-nvim-lua',
         -- for LuaSnip
         'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
@@ -31,17 +29,29 @@ return {
                 ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
+                    elseif luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     else
                         fallback()
                     end
                 end, { 'i', 's' }),
-                ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#safely-select-entries-with-cr
+                ['<CR>'] = cmp.mapping({
+                    i = function(fallback)
+                        if cmp.visible() and luasnip.expandable() then
+                            luasnip.expand()
+                        elseif cmp.visible() and cmp.get_active_entry() then
+                            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false, })
+                        else
+                            fallback()
+                        end
+                    end,
+                    s = cmp.mapping.confirm({ select = true }),
+                    c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                }),
             },
             sources = cmp.config.sources {
                 -- the order matters!
-                { name = 'nvim_lua' },
                 { name = 'nvim_lsp', max_item_count = 20 },
                 { name = 'path' },
                 { name = 'luasnip' },
