65c438a9d191275e9d74bebbcd52b565a6883782
fei6409 <fei6409@gmail.com>
2024-08-28 22:15:07 +0800

    fix(init); No check for Lua config file changes

--- init.lua
+++ init.lua
@@ -31,4 +31,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup 'plugins'
+require('lazy').setup {
+    spec = {
+        { import = 'plugins' },
+    },
+    change_detection = {
+        -- no check for config file changes
+        enabled = false,
+    },
+}
