4e8fc8147a2ac55a14046ea147b8fcd9018fd787
fei6409 <fei6409@gmail.com>
2024-10-12 17:26:48 +0800

    update(alias): kernel upstream

--- alias.zsh
+++ alias.zsh
@@ -113,7 +113,7 @@ if ([[ -f /etc/lsb-release ]] && grep "GOOGLE_ID=Goobuntu" /etc/lsb-release >/de
   alias rs='repo sync .'
   alias rsa='repo sync -j 16 -n --optimized-fetch && repo sync -j 16 -l'
 
-  alias upstream='cd ${HOME}/chromiumos/src/third_party/kernel/upstream'
+  alias kerup='cd ${HOME}/chromiumos/src/third_party/kernel/upstream'
   alias ker419='cd ${HOME}/chromiumos/src/third_party/kernel/v4.19'
   alias ker54='cd ${HOME}/chromiumos/src/third_party/kernel/v5.4'
   alias ker510='cd ${HOME}/chromiumos/src/third_party/kernel/v5.10'
