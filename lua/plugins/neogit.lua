359dd41f83fe5b90cc621c231a12a4860969cd25
fei6409 <fei6409@gmail.com>
2024-10-12 17:27:17 +0800

    feat(plugins): Add Neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+-- Git interface for Neovim
+-- https://github.com/NeogitOrg/neogit
+return {
+    'NeogitOrg/neogit',
+    dependencies = {
+        'nvim-lua/plenary.nvim', -- required
+        'sindrets/diffview.nvim', -- optional - Diff integration
+        'nvim-telescope/telescope.nvim', -- optional
+    },
+    cmd = { 'Neogit' },
+    keys = {
+        { '<leader>g', '<cmd>Neogit<CR>', desc = 'Open Neogit' },
+    },
+    opts = {
+        graph_style = 'unicode',
+    },
+}
