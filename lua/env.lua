1f99d8bd81669fed58f17ddd55a1e7c2e2f1505b
fei6409 <fei6409@gmail.com>
2024-12-11 15:07:36 +0800

    feat(env): Add autocmd to highlight on yank

--- lua/env.lua
+++ lua/env.lua
@@ -255,3 +255,10 @@ autocmd('BufWritePre', {
         vim.cmd [[keeppatterns %s/\s\+$//e]]
     end,
 })
+autocmd('TextYankPost', {
+    group = augroup,
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
