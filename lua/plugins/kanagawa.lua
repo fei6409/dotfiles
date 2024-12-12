adf926700221b9e6416c882fd4f08776468e6bd1
fei6409 <fei6409@gmail.com>
2024-12-13 00:28:26 +0800

    style(plugins): Update Telescope border theme

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -18,14 +18,13 @@ return {
                 local theme = colors.theme
                 -- magic overrides!
                 return {
-                    -- borderless Telescope
                     TelescopeTitle = { fg = theme.ui.special, bold = true },
                     TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePromptBorder = { bg = theme.ui.bg_p1, fg = theme.ui.nontext },
+                    TelescopeResultsNormal = { bg = theme.ui.bg_m1, fg = theme.ui.fg_dim },
+                    TelescopeResultsBorder = { bg = theme.ui.bg_m1, fg = theme.ui.nontext },
                     TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.nontext },
                 }
             end,
         }
