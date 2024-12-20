6fcdb1bf5722bd105c8e5636f259b7ad38766eea
fei6409 <fei6409@gmail.com>
2024-12-20 22:26:22 +0800

    feat(plugins): Add marks.nvim

---
+++ lua/plugins/marks.lua
@@ -0,0 +1,9 @@
+-- Better Vim mark interaction
+-- https://github.com/chentoast/marks.nvim
+return {
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
+    opts = {
+        refresh_interval = 1000,
+    },
+}
