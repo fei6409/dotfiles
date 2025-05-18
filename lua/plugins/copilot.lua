97cc2d714c8e3a79da8addd2b76e404271cb2522
fei6409 <fei6409@gmail.com>
2025-05-19 00:57:24 +0900

    feat(plugins): add Copilot.lua

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,21 @@
+-- Copilot support
+-- https://github.com/zbirenbaum/copilot.lua
+return {
+    'zbirenbaum/copilot.lua',
+    cmd = { 'Copilot' },
+    event = 'VeryLazy',
+    keys = {
+        { '<leader>cp', '<cmd>Copilot panel<CR>', desc = 'Open Copilot panel' },
+    },
+    opts = {
+        suggestion = {
+            keymap = {
+                accept = '<C-CR>',
+            },
+        },
+        filetypes = {
+            gitcommit = true,
+            gitrebase = true,
+        },
+    },
+}
