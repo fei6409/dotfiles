d8a651460049491f4aba36f1c5bbedb13e27183f
fei6409 <fei6409@gmail.com>
2025-06-06 14:47:25 +0800

    fix(plugins): Update which-key keymap

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,4 +4,13 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {},
+    keys = {
+        {
+            '<leader>?',
+            function()
+                require('which-key').show { global = false }
+            end,
+            desc = 'which-key: Buffer Local Keymaps',
+        },
+    },
 }
