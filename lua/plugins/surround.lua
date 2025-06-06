03473ee1bea4765ef0914cd6b5e8522dd044caa0
fei6409 <fei6409@gmail.com>
2025-06-06 15:17:49 +0800

    fix(plugins): Rework surround plugins
    Migrate to mini.surround.

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,16 +1,31 @@
--- Add surrounding pairs (quotes, braces)
--- https://github.com/kylechui/nvim-surround
+-- Surrounding actions (parenthesis, quotes, etc.)
+-- https://github.com/echasnovski/mini.surround
 return {
-    'kylechui/nvim-surround',
-    version = '*',
-    event = 'VeryLazy',
+    'echasnovski/mini.surround',
+    keys = function(_, keys)
+        -- Populate the keys based on the user's options
+        local opts = require('mini.surround').config
+        local mappings = {
+            { opts.mappings.add, desc = 'Surround: Add', mode = { 'n', 'v' } },
+            { opts.mappings.delete, desc = 'Surround: Delete' },
+            { opts.mappings.find, desc = 'Surround: Find Right' },
+            { opts.mappings.find_left, desc = 'Surround: Find Left' },
+            { opts.mappings.highlight, desc = 'Surround: Highlight' },
+            { opts.mappings.replace, desc = 'Surround: Replace' },
+        }
+        mappings = vim.tbl_filter(function(m)
+            return m[1] and #m[1] > 0
+        end, mappings)
+        return vim.list_extend(mappings, keys)
+    end,
     opts = {
-        -- `ys` is too far :p
-        keymaps = {
-            normal = 'ts',
-            normal_cur = 'tss',
-            normal_line = 'tS',
-            normal_cur_line = 'tSS',
+        mappings = {
+            add = 'gsa', -- Add surrounding in Normal and Visual modes
+            delete = 'gsd', -- Delete surrounding
+            find = 'gsf', -- Find surrounding (to the right)
+            find_left = 'gsF', -- Find surrounding (to the left)
+            highlight = 'gsh', -- Highlight surrounding
+            replace = 'gsr', -- Replace surrounding
         },
     },
 }
