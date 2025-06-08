9eb52bb6ee583a9425e68c500da68ae7a4a53f19
fei6409 <fei6409@gmail.com>
2025-06-08 14:44:10 +0800

    docs(plugins): marks: Add keymap docs

--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -6,4 +6,12 @@ return {
     opts = {
         refresh_interval = 1000,
     },
+    -- Keymaps:
+    -- mx              Set mark x
+    -- dmx             Delete mark x
+    -- m;              Toggle the next available mark at the current line
+    -- dm-             Delete all marks on the current line
+    -- dm<space>       Delete all marks in the current buffer
+    -- m]              Move to next mark
+    -- m[              Move to previous mark
 }
