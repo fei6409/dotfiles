86dca47f0430a7e3b9c08b25d60e6d7571d9737c
fei6409 <fei6409@gmail.com>
2025-07-01 00:15:29 +0800

    fix(plugins): Update Flash options and keymaps

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,16 @@
 -- https://github.com/folke/flash.nvim
 return {
     'folke/flash.nvim',
-    opts = {},
+    opts = {
+        label = {
+            -- disable uppercase labels
+            uppercase = false,
+        },
+        modes = {
+            -- disable `f`, `F`, `t`, `T`, `;` and `,` motions
+            char = { enabled = false },
+        },
+    },
     keys = {
         {
             '<C-f>',
@@ -19,6 +28,14 @@ return {
                 require('flash').toggle()
             end,
             desc = 'Flash: Toggle Search',
+        },
+        {
+            '<C-t>',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').treesitter_search()
+            end,
+            desc = 'Flash: Treesitter Search',
         },
     },
 }
