9b0a1cd101c35971975de8f3bf698bb17f6795ad
fei6409 <fei6409@gmail.com>
2025-07-31 01:13:12 +0800

    feat(plugins): gitsigns: Add keymaps for hunk and blame operations

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,5 +3,17 @@
 return {
     'lewis6991/gitsigns.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+        -- Use committer_time instead of author_time
+        current_line_blame_formatter = '<abbrev_sha> <author> (<committer_time:%R>) | <summary>',
+    },
+    keys = {
+        { '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', desc = 'Gitsigns: Stage hunk' },
+        { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<cr>', desc = 'Gitsigns: Unstage hunk' },
+        { '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>', desc = 'Gitsigns: Reset hunk' },
+        { '<leader>hp', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Gitsigns: Preview hunk' },
+        { '<leader>hb', '<cmd>Gitsigns blame<cr>', desc = 'Gitsigns: Blame' },
+        { '<leader>hl', '<cmd>Gitsigns blame_line<cr>', desc = 'Gitsigns: Blame line' },
+        { '<leader>ht', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Gitsigns: Toggle inline blame' },
+    },
 }
