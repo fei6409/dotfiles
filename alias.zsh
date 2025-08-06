5819430bc93001769916ce96d53fa7bdbf43111d
fei6409 <fei6409@gmail.com>
2025-08-06 12:57:01 +0800

    chore(alias): Update 'cr'

--- alias.zsh
+++ alias.zsh
@@ -154,7 +154,7 @@ if ([[ -f /etc/lsb-release ]] && grep "GOOGLE_ID=Goobuntu" /etc/lsb-release >/de
 
     else
         # Only allowed outside of CrOS chroot
-        alias cr='cd ${HOME}/chromiumos/src; cros_sdk --no-ns-pid --no-update'
+        alias cr='cd ${HOME}/chromiumos/src/scripts; cros_sdk --no-ns-pid --no-update'
         alias enter-chrome-sdk="cros chrome-sdk --board=${BOARD} --log-level=info --internal \
           --gn-extra-args='enable_nacl=false symbol_level=1 use_remoteexec=true dcheck_always_on=false is_official_build=false'"
     fi
