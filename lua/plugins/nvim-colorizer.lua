24c615055c19d1debb2c2a650b63b0268c93e5ed
fei6409 <fei6409@gmail.com>
2025-08-12 00:15:30 +0800

    feat(plugins): Add nvim-colorizer.lua

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,10 @@
+-- Highlight color code
+-- https://github.com/catgoose/nvim-colorizer.lua
+return {
+    'catgoose/nvim-colorizer.lua',
+    opts = {
+        user_default_options = {
+            xterm = true,
+        },
+    },
+}
