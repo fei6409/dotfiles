a929a4b52ef23cd7772a21a2e99bc7af27a47e77
fei6409 <fei6409@gmail.com>
2025-08-15 15:03:47 +0800

    feat(plugins): enable Neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,15 +4,21 @@ return {
     'NeogitOrg/neogit',
     dependencies = {
         'nvim-lua/plenary.nvim', -- required
-        'sindrets/diffview.nvim', -- optional - Diff integration
+        -- 'sindrets/diffview.nvim', -- optional - Diff integration
         'nvim-telescope/telescope.nvim', -- optional
     },
     cmd = { 'Neogit' },
     keys = {
-        { '<leader>g', '<cmd>Neogit<cr>', desc = 'Open Neogit' },
+        { '<leader>g', '<cmd>Neogit<cr>', desc = 'Neogit' },
     },
     opts = {
         graph_style = 'unicode',
+        integrations = {
+            -- use telescope for menu selection rather than vim.ui.select.
+            telescope = true,
+        },
+        commit_editor = {
+            staged_diff_split_kind = 'auto',
+        },
     },
-    enabled = false,
 }
