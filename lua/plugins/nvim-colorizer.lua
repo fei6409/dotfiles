f5e1d2953fb419e55bb429866f5ef0d22cc213b5
fei6409 <fei6409@gmail.com>
2025-08-18 11:46:33 +0800

    fix(plugins): limit colorizer to particular filetypes

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -3,6 +3,16 @@
 return {
     'catgoose/nvim-colorizer.lua',
     opts = {
+        filetypes = {
+            'css',
+            'html',
+            'log',
+            'lua',
+            'markdown',
+            'sh',
+            'vim',
+            'zsh',
+        },
         user_default_options = {
             xterm = true,
         },
