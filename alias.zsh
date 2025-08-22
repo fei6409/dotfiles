5fe49694312f648c845ab9e978cec705ceee803a
fei6409 <fei6409@gmail.com>
2025-08-22 00:34:03 +0000

    feat(alias): rsc => repo sync current-branch
    For AOSP

--- alias.zsh
+++ alias.zsh
@@ -108,6 +108,7 @@ if ([[ -f /etc/lsb-release ]] && grep "GOOGLE_ID=Goobuntu" /etc/lsb-release >/de
 
     alias rs='repo sync .'
     alias rsa='repo sync -j 16 -n --optimized-fetch && repo sync -j 16 -l'
+    alias rsc='repo sync -j 16 -n --optimized-fetch -c && repo sync -j 16 -l'
 
     alias kerup='cd ${HOME}/chromiumos/src/third_party/kernel/upstream'
     alias ker419='cd ${HOME}/chromiumos/src/third_party/kernel/v4.19'
