8f493ac550a8c45ed8e3c3e16312211496c5103c
fei6409 <fei6409@gmail.com>
2026-01-30 09:58:57 +0800

    fix(alias): Update repo aliases

--- alias.zsh
+++ alias.zsh
@@ -129,9 +129,9 @@ fi
 if ([[ -f /etc/lsb-release ]] && grep "GOOGLE_ID=Goobuntu" /etc/lsb-release >/dev/null) ||
     [[ -n $CROS_WORKON_SRCROOT ]]; then
 
-    alias rs='repo sync -j 16 --no-interleaved'
-    alias rsa='rs -n --optimized-fetch && rs --auto-gc -l'
-    alias rsc='rs -n --optimized-fetch -c && rs --auto-gc -l'
+    alias rs='repo sync -j 16'
+    alias rsa='rs -n --optimized-fetch && rs -l'
+    alias rsc='rs -n --optimized-fetch -c && rs -l'
 
     alias kerup='cd ${HOME}/chromiumos/src/third_party/kernel/upstream'
     alias ker54='cd ${HOME}/chromiumos/src/third_party/kernel/v5.4'
