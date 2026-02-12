c9c0e61236c7da5d8350cd61662dc2ee1d36bf86
fei6409 <fei6409@gmail.com>
2026-02-12 22:10:23 +0800

    feat(lua): add nvim-lint

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,26 @@
+-- lightweight linter
+-- https://github.com/mfussenegger/nvim-lint
+return {
+    'mfussenegger/nvim-lint',
+    event = 'VeryLazy',
+    config = function()
+        local lint = require('lint')
+
+        -- ignore undefined variable 'vim'
+        lint.linters.luacheck.args = {
+            '--globals',
+            'vim',
+        }
+
+        -- skip shellcheck as bashls runs it
+        lint.linters_by_ft = {
+            lua = { 'luacheck' },
+            markdown = { 'vale' },
+        }
+
+        vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+            group = vim.api.nvim_create_augroup('fei_lint', { clear = true }),
+            callback = function() lint.try_lint() end,
+        })
+    end,
+}
