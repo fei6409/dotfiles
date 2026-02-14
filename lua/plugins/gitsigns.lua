a8497456f03750d26177fb940abf5964a14ba8b4
fei6409 <fei6409@gmail.com>
2026-02-14 17:10:43 +0800

    feat(gitsigns): add hunk navigation keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,8 +10,10 @@ return {
     keys = {
         { '<leader>hs', '<cmd>Gitsigns stage_hunk<cr>', desc = 'Gitsigns: Stage hunk' },
         { '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<cr>', desc = 'Gitsigns: Unstage hunk' },
+        { '<leader>hn', '<cmd>Gitsigns next_hunk<cr>', desc = 'Gitsigns: Next hunk' },
+        { '<leader>hp', '<cmd>Gitsigns prev_hunk<cr>', desc = 'Gitsigns: Previous hunk' },
         { '<leader>hr', '<cmd>Gitsigns reset_hunk<cr>', desc = 'Gitsigns: Reset hunk' },
-        { '<leader>hp', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Gitsigns: Preview hunk' },
+        { '<leader>hv', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Gitsigns: View hunk' },
         { '<leader>hb', '<cmd>Gitsigns blame<cr>', desc = 'Gitsigns: Blame' },
         { '<leader>hl', '<cmd>Gitsigns blame_line<cr>', desc = 'Gitsigns: Blame line' },
         { '<leader>ht', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Gitsigns: Toggle inline blame' },
