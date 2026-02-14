ea65171d8619278556999498e3b8da838a695cc4
fei6409 <fei6409@gmail.com>
2026-02-14 23:00:50 +0800

    feat(render-markdown): add keymap for toggling preview

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,6 +3,10 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    keys = {
+        { '<leader>p', '<cmd>RenderMarkdown preview<cr>', ft = 'markdown', desc = 'Render Markdown preview' },
+    },
     opts = {
         completions = { lsp = { enabled = true } },
     },
