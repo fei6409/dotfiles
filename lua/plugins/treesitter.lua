2d5964fab73609dcd09705292ef99391040ac445
fei6409 <fei6409@gmail.com>
2026-08-28 19:18:10 +0800

    lua: treesitter: Skip highlight for large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -30,7 +30,12 @@ return {
         require('nvim-treesitter').install(filetypes)
         vim.api.nvim_create_autocmd('FileType', {
             pattern = filetypes,
-            callback = function() vim.treesitter.start() end,
+            callback = function(args)
+                local max_filesize = 100 * 1024 -- 100 KB
+                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+                if ok and stats and stats.size > max_filesize then return end
+                vim.treesitter.start()
+            end,
         })
     end,
 }
